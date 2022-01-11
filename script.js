f87b67213d7b0ba62c7bdef195023167824fc3b8
jbravoh <jbravoharriott@gmail.com>
2022-01-11 09:31:21 +0000

    Created Todo List project

---
+++ script.js
@@ -0,0 +1,54 @@
+// =====> ADD A NEW TODO
+
+const addForm = document.querySelector(".add");
+const list = document.querySelector(".todos");
+const search = document.querySelector(".search input");
+
+// create a reusable function
+
+const generateTemplate = (todo) => {
+  const html = `  
+   <li class="list-group-item d-flex justify-content-between align-items-center">
+    <span>${todo}</span>
+    <i class="far fa-trash-alt delete"></i>
+  </li>
+  `;
+
+  list.innerHTML += html;
+};
+
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todo = addForm.add.value.trim();
+  // .trim() takes away whitespace
+
+  if (todo.length) {
+    generateTemplate(todo);
+    addForm.reset(); // resets the input fields inside the form
+  }
+});
+
+// =====> DELETE TODOS - use event delegation
+
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+  }
+});
+
+// =====> FILTER TODOS - keyup event
+
+const filterTodos = (term) => {
+  Array.from(list.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.remove("filtered"));
+
+  Array.from(list.children)
+    .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.add("filtered"));
+};
+
+search.addEventListener("keyup", () => {
+  const term = search.value.trim().toLowerCase();
+  filterTodos(term);
+});
